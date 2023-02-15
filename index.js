768a405cdef86e0ee8a3340ffb425740f69b4ebc
Mar Kal <marc@example.com>
2023-02-15 20:36:50 +0100

    feat: finish routes and models configuration

--- index.js
+++ index.js
@@ -1,44 +1,24 @@
 const http = require('http');
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const logger = require('./utilis/logger')
-const config = require('./utilis/config')
-
 const app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const logger = require('./utilis/logger');
+const config = require('./utilis/config');
+const mongoose = require('mongoose');
+
+logger.info('connecting to database');
 
 mongoose.set('strictQuery', true);
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl).then(() => {
+mongoose.connect(config.MONGODB_URI).then(() => {
   logger.info('connected to database');
 });
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
