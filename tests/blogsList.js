addfb3a8d4efe608a18bf99a09b542712ab59fe6
Mar Kal <marc@example.com>
2023-02-17 16:39:31 +0100

    feat: finish 4.5 task

---
+++ tests/blogsList.js
@@ -0,0 +1,33 @@
+const listWithNoBlogs = [];
+
+const listWithOneBlog = [
+  {
+    title: 'Test tile',
+    author: 'Test author',
+    likes: 2
+  }
+];
+
+const blogs = [
+  {
+    title: 'Test tile',
+    author: 'Test author',
+    likes: 2
+  },
+  {
+    title: 'Test tile2',
+    author: 'Test author2',
+    likes: 22
+  },
+  {
+    title: 'Test tile3',
+    author: 'Test author3',
+    likes: 7
+  }
+];
+
+module.exports = {
+  listWithNoBlogs,
+  listWithOneBlog,
+  blogs
+};
--- utilis/list_helper.js
+++ utilis/list_helper.js
@@ -8,7 +8,22 @@ const totalLikes = (blogs) => {
     : blogs.map((prop) => prop.likes).reduce((prev, next) => prev + next);
 };
 
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 'There is no blogs';
+  }
+
+  if (blogs.length === 1) {
+    return blogs;
+  }
+
+  const maxLikes = Math.max(...blogs.map((prop) => prop.likes));
+  const result = blogs.find((blog) => blog.likes === maxLikes);
+  return result;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  mostLikes
 };
